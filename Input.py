dad79f35cede579d539450a9d41d13826f38ac21
KayapatiJahnavi <kayapatijahnavi@gmail.com>
2025-09-19 23:55:12 +0530

    Create Input.py

---
+++ Input.py
@@ -0,0 +1,3 @@
+favorite_food= input( "enter your favorite food")
+favorite_drink=input(" entre your favorite drink")
+print(f" Your favorite food is {favorite_food} and  your favorite drink is {favorite_drink}")
